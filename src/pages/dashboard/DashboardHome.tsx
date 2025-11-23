8d80a6e16357023bf2fcfc75d4109e6097976aba
keianrpo-ops <keianrpo@gmail.com>
2025-11-23 03:32:33 -0500

    Deploy automático

--- src/pages/dashboard/DashboardHome.tsx
+++ src/pages/dashboard/DashboardHome.tsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import Layout from '../../components/Layout';
 import DateRangeBar from '../../components/DateRangeBar';
-import KpiGrid from '../../components/KpiGrid'; // Mantenemos la importación aunque no se use en el return
+
 import TopAdsTable from '../../components/TopAdsTable';
 import { fetchSheet } from '../../services/googleSheetsService';
 // Eliminamos la importación de kpisMock, ya que causaba el error TS6133
